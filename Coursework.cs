473dc3d7fe07542a0c872f57b699cf8a294b3726
Zheng Wei Ng <eo99x6@gmail.com>
2024-05-06 13:26:44 -0700

    hs

--- Coursework.cs
+++ Coursework.cs
@@ -1,7 +1,9 @@
-public abstract class Coursework() {
+public abstract class Coursework(float maxGrade = 100)
+{
+    public float GradeEarned { get; protected set; }
+    public float MaxGrade { get; protected set; } = maxGrade;
     public DateTime? DueAt { get; protected set; }
     public DateTime? UnlockAt { get; protected set; }
     public DateTime? LockAt { get; protected set; }
-    
-    
+
 }
--- Quiz.cs
+++ Quiz.cs
@@ -1,3 +1,8 @@
+using MockCanvas.Questions;
 public class Quiz() : Coursework {
-    
+    private QuestionSet questionSet;
+    public Quiz(QuestionSet questionSet) : this()
+    {
+        this.questionSet = questionSet;
+    }
 }
